89c6b0ae27ee8ed05b1d67f051c1d2699676d581
David Dong <daviddong201712@gmail.com>
2020-08-24 08:47:50 -0500

    After merge the remote github readme.md, modifies babysitter path in local repository, makes a commit.

--- src/main/java/org/launchcode/shareservice/controllers/NeedsBabysitterController.java
+++ src/main/java/org/launchcode/shareservice/controllers/NeedsBabysitterController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("needsbabysitter")
+@RequestMapping("babysitter")
 public class NeedsBabysitterController {
 
     @GetMapping("index")
     public String displayMainPage(Model model) {
-        return "needsbabysitter/index";
+        return "babysitter/index";
     }
 }

7a74e3c13486b324a4116306a897746b35ad5f5b
David Dong <daviddong201712@gmail.com>
2020-08-31 12:36:45 -0500

    Adds authentication filter, customer needs to login and to create their events.

--- src/main/java/org/launchcode/shareservice/controllers/HomeController.java
+++ src/main/java/org/launchcode/shareservice/controllers/HomeController.java
@@ -3,8 +3,10 @@ package org.launchcode.shareservice.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("mainpage")
 public class HomeController {
 
     @GetMapping

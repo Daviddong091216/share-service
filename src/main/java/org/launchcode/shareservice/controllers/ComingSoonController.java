9db955c38a038fd978e17d2b8b94784e7951c6c9
David Dong <daviddong201712@gmail.com>
2020-08-30 21:27:00 -0500

    Modifies coming soon template.

--- src/main/java/org/launchcode/shareservice/controllers/ComingSoonController.java
+++ src/main/java/org/launchcode/shareservice/controllers/ComingSoonController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("babysitter")
-public class NeedsBabysitterController {
+@RequestMapping("comingsoon")
+public class ComingSoonController {
 
     @GetMapping("index")
     public String displayMainPage(Model model) {
-        return "babysitter/index";
+        return "comingsoon/index";
     }
+
 }

31f886d1960fb0b25fc38dadf20d51970b411fa8
David Dong <daviddong201712@gmail.com>
2020-09-15 10:57:37 -0500

    Adds JavaScript code. Creates recreation features on the main page.

--- src/main/java/org/launchcode/shareservice/models/Problem.java
+++ src/main/java/org/launchcode/shareservice/models/Problem.java
@@ -8,28 +8,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-public class Problem extends AbstractEntity{
-
-    @NotNull(message = "Description is required!")
-    @Size(min = 3, max = 500, message = "Description must be between 3 and 500 characters.")
-    private String description;
+public class Problem extends AbstractEntity {
 
     @ManyToMany(mappedBy = "problems")
-    private final List<AcNeedsRepairing> acNeedsRepairings= new ArrayList<>();
-
-    public Problem(String description) {
-        this.description = description;
-    }
+    private final List<AcNeedsRepairing> acNeedsRepairings = new ArrayList<>();
 
     public Problem() {
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
     }
 
     public List<AcNeedsRepairing> getAcNeedsRepairings() {

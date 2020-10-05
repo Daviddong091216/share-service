1c89f0d74f4e48acfb823c11f4f15d8a8d79df0f
David Dong <daviddong201712@gmail.com>
2020-10-05 13:24:20 -0500

    Adds update feature.

--- src/main/java/org/launchcode/shareservice/controllers/AcNeedsRepairingController.java
+++ src/main/java/org/launchcode/shareservice/controllers/AcNeedsRepairingController.java
@@ -9,7 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
@@ -32,17 +34,17 @@ public class AcNeedsRepairingController {
     @Autowired
     private ZipCodeRepository zipCodeRepository;
 
-    @GetMapping("add")
+    @GetMapping("create")
     public String displayAddForm(Model model) {
         model.addAttribute("title", "AC Needs Repairing");
         model.addAttribute("acNeedsRepairing", new AcNeedsRepairing());
         model.addAttribute("states", stateRepository.findAll());
         model.addAttribute("zipCodes", zipCodeRepository.findAll());
         model.addAttribute("problems", problemRepository.findAll());
-        return "/acNeedsRepairing/add";
+        return "/acNeedsRepairing/create-or-update";
     }
 
-    @PostMapping("add")
+    @PostMapping("create")
 //    we must put Errors errors parameter directly after the Job parameter for it to work properly
     public String processAddForm(@ModelAttribute @Valid AcNeedsRepairing newAcNeedsRepairing,
                                     Errors errors,
@@ -54,7 +56,7 @@ public class AcNeedsRepairingController {
             model.addAttribute("states", stateRepository.findAll());
             model.addAttribute("zipCodes", zipCodeRepository.findAll());
             model.addAttribute("problems", problemRepository.findAll());
-            return "/acNeedsRepairing/add";
+            return "/acNeedsRepairing/create-or-update";
         }
 
         if (errors.hasErrors()) {
@@ -62,7 +64,7 @@ public class AcNeedsRepairingController {
             model.addAttribute("states", stateRepository.findAll());
             model.addAttribute("zipCodes", zipCodeRepository.findAll());
             model.addAttribute("problems", problemRepository.findAll());
-            return "/acNeedsRepairing/add";
+            return "/acNeedsRepairing/create-or-update";
         }
 
 
@@ -108,5 +110,62 @@ public class AcNeedsRepairingController {
             return "redirect:../";
         }
     }
+
+
+    @GetMapping(value = "update/{acNeedsRepairingId}")
+    public String displayUpdateEventForm(@PathVariable int acNeedsRepairingId, Model model, HttpServletRequest request) {
+
+        model.addAttribute("title", "Update AC Needs Repairing");
+//        model.addAttribute("actionUrl", request.getRequestURI());
+
+        Optional<AcNeedsRepairing> event = acNeedsRepairingRepository.findById(acNeedsRepairingId);
+        if (event.isPresent()) {
+            model.addAttribute("acNeedsRepairing",event.get() );
+            model.addAttribute("states", stateRepository.findAll());
+            model.addAttribute("zipCodes", zipCodeRepository.findAll());
+            model.addAttribute("problems", problemRepository.findAll());
+        }
+//        else {
+//            model.addAttribute(MESSAGE_KEY, "warning|No event found with id: " + Integer.toString(acNeedsRepairingId));
+//        }
+
+        return "acNeedsRepairing/create-or-update";
+    }
+
+    @PostMapping(value = "update/{acNeedsRepairingId}")
+    public String processUpdateForm(@ModelAttribute @Valid AcNeedsRepairing newAcNeedsRepairing,
+                                 Errors errors,
+                                 @RequestParam int state,
+                                 @RequestParam List<Integer> problems,
+                                 Model model) {
+        if(problems.isEmpty()){
+            model.addAttribute("title", "AC Needs Repairing");
+            model.addAttribute("states", stateRepository.findAll());
+            model.addAttribute("zipCodes", zipCodeRepository.findAll());
+            model.addAttribute("problems", problemRepository.findAll());
+            return "/acNeedsRepairing/create-or-update";
+        }
+
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "AC Needs Repairing");
+            model.addAttribute("states", stateRepository.findAll());
+            model.addAttribute("zipCodes", zipCodeRepository.findAll());
+            model.addAttribute("problems", problemRepository.findAll());
+            return "/acNeedsRepairing/create-or-update";
+        }
+
+
+        Optional optState = stateRepository.findById(state);
+        if (optState.isPresent()) {
+            State aState = (State) optState.get();
+            model.addAttribute("state", aState);
+            acNeedsRepairingRepository.save(newAcNeedsRepairing);
+            return "/acNeedsRepairing/view";
+        } else {
+            return "redirect:../";
+        }
+    }
+
+
 
 }
